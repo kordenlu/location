d005c6690cdc8b01391683a18b4fa59a9b85e7e3
jimingmin <jimingmin@gmail.com>
2015-03-29 22:37:13 +0800

    完成逻辑

--- logic/getstationuserlist_handler.cpp
+++ logic/getstationuserlist_handler.cpp
@@ -22,6 +22,8 @@
 #include "../server_typedef.h"
 #include "../bank/redis_bank.h"
 #include "../bank/mongo_bank.h"
+#include "../../rapidjson/document.h"
+using namespace rapidjson;
 
 using namespace LOGGER;
 using namespace FRAME;
@@ -49,7 +51,7 @@ int32_t CGetStationUserListHandler::GetStationUserList(ICtlHead *pCtlHead, IMsgH
 	uint64_t nLongtitude;
 	uint64_t nLatitude;
 	CBusConfig *pBusConfig = (CBusConfig *)g_Frame.GetConfig(CONFIG_BUS);
-	pBusConfig->GetStationPosition(pGetStationUserListReq->m_strCityName, pGetStationUserListReq->m_strBusLineID, pGetStationUserListReq->m_strStationName,
+	pBusConfig->GetStationPosition(pGetStationUserListReq->m_strBusLineID, pGetStationUserListReq->m_strStationName,
 			nLongtitude, nLatitude);
 
 	CMongoBank *pMongoBank = (CMongoBank *)g_Frame.GetBank(BANK_MONGO);
@@ -57,16 +59,111 @@ int32_t CGetStationUserListHandler::GetStationUserList(ICtlHead *pCtlHead, IMsgH
 	mongoc_client_t *pMongoClient = pMongoBank->GetMongoChannel("location");
 	mongoc_collection_t *pCollection = mongoc_client_get_collection(pMongoClient, "location", "user_coord");
 
-	bson_t *query = BCON_NEW("position", "{", "$near", "[", BCON_DOUBLE(double(nLongtitude / 1000000)),
-			BCON_DOUBLE(double(nLatitude / 1000000)), "]", "$maxDistance", BCON_DOUBLE(0.1), "}");
+	bson_t *query = BCON_NEW("geoNear", "user_coord", "near", "[", BCON_DOUBLE(nLatitude / 1000000.0),
+			BCON_DOUBLE(nLongtitude / 1000000.0), "]", "maxDistance", BCON_DOUBLE(0.68 / 111.0), "query",
+			"{", "updatetime", "{", "$gt", BCON_INT32(0), "}", "}");
 
-	mongoc_cursor_t *cursor = mongoc_collection_find(pCollection, MONGOC_QUERY_NONE, 0, 0, 0, query, NULL, NULL);
+	mongoc_cursor_t *cursor = mongoc_collection_command(pCollection, MONGOC_QUERY_NONE, 0, 0, 0, query, NULL, NULL);
+
+	const Station *pPrevStation = pBusConfig->GetPrevStation(pGetStationUserListReq->m_strBusLineID, pGetStationUserListReq->m_strStationName);
+	const Station *pNextStation = pBusConfig->GetNextStation(pGetStationUserListReq->m_strBusLineID, pGetStationUserListReq->m_strStationName);
+
+	CGetStationUserListResp stGetStationUserListResp;
+	stGetStationUserListResp.m_nResult = CGetStationUserListResp::enmResult_OK;
+	stGetStationUserListResp.m_strPrevStation = (pPrevStation == NULL ? pGetStationUserListReq->m_strStationName : pPrevStation->m_strStationName);
+	stGetStationUserListResp.m_strNextStation = (pNextStation == NULL ? pGetStationUserListReq->m_strStationName : pNextStation->m_strStationName);
+	stGetStationUserListResp.m_nUserCount = 0;
+	stGetStationUserListResp.m_nNearUserCount = 0;
+
+	int32_t nUserCount = 0;
+	int32_t nNearUserCount = 0;
 	const bson_t *doc;
 	while(mongoc_cursor_next(cursor, &doc))
 	{
 		char *str = bson_as_json(doc, NULL);
+		Document document;
+		if(document.Parse<0>(str).HasParseError())
+		{
+			bson_free(str);
+			continue;
+		}
+
+		if(!document.IsObject())
+		{
+			bson_free(str);
+			continue;
+		}
+
+		Value &stResult = document["results"];
+		if(!stResult.IsArray())
+		{
+			bson_free(str);
+			continue;
+		}
+
+		for(SizeType i = 0; i < stResult.Size(); ++i)
+		{
+			if(!stResult[i].IsObject())
+			{
+				break;
+			}
+
+			Value &stElement = stResult[i];
+			double nDistance = stElement["dis"].GetDouble() * 111.0;
+
+			Value &stObj = stElement["obj"];
+			if(!stObj.IsObject())
+			{
+				break;
+			}
+
+			//大于0.5km
+			if(nDistance * 10 > 5)
+			{
+				stGetStationUserListResp.m_arrNearUserSimpleInfo[nNearUserCount].m_nUin = stObj["uin"].GetUint();
+				stGetStationUserListResp.m_arrNearUserSimpleInfo[nNearUserCount].m_strNickName = stObj["nickname"].GetString();
+				stGetStationUserListResp.m_arrNearUserSimpleInfo[nNearUserCount].m_strHeadImageAddr = stObj["headimageaddr"].GetString();
+				stGetStationUserListResp.m_arrNearUserSimpleInfo[nNearUserCount].m_nAge = stObj["age"].GetInt();
+				stGetStationUserListResp.m_arrNearUserSimpleInfo[nNearUserCount].m_nGender = stObj["gender"].GetInt();
+				stGetStationUserListResp.m_arrNearUserSimpleInfo[nNearUserCount].m_strOneselfWords = stObj["oneselfwords"].GetString();
+
+				++nNearUserCount;
+			}
+			else
+			{
+				stGetStationUserListResp.m_arrUserSimpleInfo[nUserCount].m_nUin = stObj["uin"].GetUint();
+				stGetStationUserListResp.m_arrUserSimpleInfo[nUserCount].m_strNickName = stObj["nickname"].GetString();
+				stGetStationUserListResp.m_arrUserSimpleInfo[nUserCount].m_strHeadImageAddr = stObj["headimageaddr"].GetString();
+				stGetStationUserListResp.m_arrUserSimpleInfo[nUserCount].m_nAge = stObj["age"].GetInt();
+				stGetStationUserListResp.m_arrUserSimpleInfo[nUserCount].m_nGender = stObj["gender"].GetInt();
+				stGetStationUserListResp.m_arrUserSimpleInfo[nUserCount].m_strOneselfWords = stObj["oneselfwords"].GetString();
+
+				++nUserCount;
+			}
+		}
+
 		bson_free(str);
 	}
+
+	stGetStationUserListResp.m_nUserCount = nUserCount;
+	stGetStationUserListResp.m_nNearUserCount = nNearUserCount;
+
+	CMsgDispatchConfig *pMsgDispatchConfig = (CMsgDispatchConfig *)g_Frame.GetConfig(CONFIG_MSGDISPATCH);
+	CRedisBank *pRedisBank = (CRedisBank *)g_Frame.GetBank(BANK_REDIS);
+	CRedisChannel *pRespChannel = pRedisBank->GetRedisChannel(pMsgDispatchConfig->GetChannelKey(MSGID_GETSTATIONUSERLIST_RESP));
+
+	uint8_t arrRespBuf[MAX_MSG_SIZE];
+
+	MsgHeadCS stMsgHeadCS;
+	stMsgHeadCS.m_nMsgID = MSGID_GETSTATIONUSERLIST_RESP;
+	stMsgHeadCS.m_nSeq = pMsgHeadCS->m_nSeq;
+	stMsgHeadCS.m_nSrcUin = pMsgHeadCS->m_nSrcUin;
+	stMsgHeadCS.m_nDstUin = pMsgHeadCS->m_nDstUin;
+
+	uint16_t nTotalSize = CServerHelper::MakeMsg(pCtlHead, &stMsgHeadCS, &stGetStationUserListResp, arrRespBuf, sizeof(arrRespBuf));
+	pRespChannel->RPush(NULL, (char *)arrRespBuf, nTotalSize);
+
+	g_Frame.Dump(pCtlHead, &stMsgHeadCS, &stGetStationUserListResp, "send ");
 
 	return 0;
 }
